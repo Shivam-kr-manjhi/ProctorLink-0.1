83d0478964901e8440e2af1f4249568fd09fab81
kumarkse <abhishektiwari2100@gmail.com>
2024-07-14 02:30:59 +0530

    all loaders done

--- static/js/stats.js
+++ static/js/stats.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     const url = '/getresult';
-    const loading = document.getElementById('load');
+    const loading = document.getElementById('loading');
     const statsContainer = document.getElementById('statscontainer');
 
     // Fetch data from the server
